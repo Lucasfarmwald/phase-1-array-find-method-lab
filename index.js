127c2324ba92fc58dde09b104b78c5237f690d0e
lucasfarmwald <farmwaldlucas@gmail.com>
2021-09-05 18:11:08 -0500

    commit tree 13c25abf4f394908c3b4004fce095328590e21ae

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-function superbowlWin(record){
-   record = []
-   return record < 'W'
+function superbowlWin(records){
+    const found = records.find(function(record){
+        return record.result === "W"
+    })
+    if(found){
+        return found.year
+    }
   }
+
+
+
+//   const array1 = [5, 12, 8, 130, 44];
+
+// const found = array1.find(element => element > 10);
+
+// console.log(found);
+// expected output: 12
